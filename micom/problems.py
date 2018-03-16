092ece1b147094e46e025510dbd3a044be5e19f7
Christian Diener <ch.diener@gmail.com>
2018-03-16 11:46:06 -0600

    Fix bug in knockouts

--- micom/problems.py
+++ micom/problems.py
@@ -110,6 +110,8 @@ def knockout_species(community, species, fraction, method, progress,
                 [r.knock_out() for r in
                  com.reactions.query(lambda ri: ri.community_id == sp)]
 
+                com.variables.community_objective.lb = 0
+                com.variables.community_objective.ub = None
                 with com:
                     com.objective = 1000.0 * com.variables.community_objective
                     min_growth = optimize_with_retry(

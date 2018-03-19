0328ea78fc4a21bf9a7f236a50d3f5c313649f9c
Christian Diener <ch.diener@gmail.com>
2018-03-19 12:09:23 -0600

    retry in knockout

--- micom/problems.py
+++ micom/problems.py
@@ -111,12 +111,13 @@ def knockout_species(community, species, fraction, method, progress,
                  com.reactions.query(lambda ri: ri.community_id == sp)]
 
                 com.variables.community_objective.lb = 0
-                com.variables.community_objective.ub = community
+                com.variables.community_objective.ub = community_min_growth
                 with com:
                     com.objective = 1000.0 * com.variables.community_objective
                     min_growth = optimize_with_retry(
                         com, message="could not get community growth rate.")
                     min_growth /= 1000.0
+                com.variables.community_objective.ub = None
                 com.variables.community_objective.lb = fraction * min_growth
                 com.variables.community_objective.ub = min_growth
                 sol = com.optimize()

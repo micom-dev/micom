53e84828b4e3a488b4a8dd0ccd29bf7c3a342232
Christian Diener <ch.diener@gmail.com>
2019-07-08 16:20:30 -0700

    update docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.3+1.g2869280',
+    VERSION: '0.9.3+7.g302cc54.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',

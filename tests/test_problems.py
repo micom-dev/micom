c2c5c6f85c06453921716ed28399003e051f7e81
Christian Diener <ch.diener@gmail.com>
2020-04-13 19:07:11 -0700

    disable cplex for large problem tests

--- tests/test_problems.py
+++ tests/test_problems.py
@@ -11,6 +11,7 @@ class TestOptcom():
 
     @pytest.mark.parametrize("strategy", strategies)
     def test_is_consistent(self, community, strategy):
+        community.solver = "glpk"
         sol = community.optcom(strategy=strategy)
         assert np.allclose(sol.growth_rate, 0.873922)
         assert np.allclose(sol.members.growth_rate.dropna().sum(), 4*0.873922)
@@ -18,17 +19,20 @@ class TestOptcom():
     @pytest.mark.parametrize("f", [0, 0.25, "0.5", [0, 0.2, 0.4, 0],
                                    np.ones(4) * 0.7])
     def test_good_mingrowth(self, community, f):
+        community.solver = "glpk"
         sol = community.optcom(strategy="lmoma", min_growth=f)
         assert np.allclose(sol.growth_rate, 0.873922)
         assert np.allclose(sol.members.growth_rate.dropna().sum(), 4*0.873922)
 
     @pytest.mark.parametrize("f", ["a", [1, 2], np.ones(6)])
     def test_bad_mingrowth(self, community, f):
+        community.solver = "glpk"
         with pytest.raises(ValueError):
             sol = community.optcom(strategy="lmoma", min_growth=f)
 
     @pytest.mark.parametrize("strategy", strategies)
     def test_must_share(self, community, strategy):
+        community.solver = "glpk"
         community.reactions.EX_glc__D_m.lower_bound = -5
         sol = community.optcom(strategy=strategy, min_growth=1.0)
         assert sol is None
@@ -37,6 +41,7 @@ class TestOptcom():
         assert (sol.members.growth_rate.dropna() > 0.4).any()
 
     def test_conservation(self, community):
+        community.solver = "glpk"
         community.reactions.EX_glc__D_m.lower_bound = -5
         sol = community.optcom(strategy="lmoma", fluxes=True)
         imports = sol["EX_glc__D_e"][0:4]

cb45166b400f28fc8e15d89cf2d5b55df9b90f97
Christian Diener <ch.diener@gmail.com>
2020-04-15 13:23:35 -0700

    update qiime data

---
+++ tests/test_numeric.py
@@ -0,0 +1,30 @@
+"""Test numerical stabilization methods."""
+
+from fixtures import community
+import micom.solution as ms
+from os import path
+from pytest import mark, raises, approx
+
+
+def test_crossover(community):
+    sol = community.cooperative_tradeoff(fraction=1.0)
+    gcs = sol.members.growth_rate.dropna()
+    sol = ms.crossover(community, sol)
+    for i, g in enumerate(gcs):
+        sol.members.growth_rate[i] == approx(g)
+
+
+def test_reset(community):
+    sol = community.optimize()
+    ms.reset_solver(community)
+    assert community.optimize().growth_rate == approx(sol.growth_rate)
+
+
+def test_retry(community):
+    sol = ms.optimize_with_retry(community)
+    assert sol == approx(0.874, 0.001)
+
+
+def test_fraction(community):
+    sol = ms.optimize_with_fraction(community, 0.5)
+    assert sol.growth_rate == approx(0.874, 0.001)
---
+++ tests/test_tradeoff.py
@@ -0,0 +1,24 @@
+"""Test cooperative tradeoff."""
+
+from fixtures import community
+import micom.qiime_formats as qf
+from os import path
+from pytest import mark, raises, approx
+
+
+def test_cooperative_tradeoff(community):
+    sol = community.cooperative_tradeoff(fraction=1.0)
+    for g in sol.members.growth_rate.dropna():
+        assert g == approx(0.874, 0.001)
+
+
+def test_multiple_tradeoffs(community):
+    fs = [1.0, 0.5, 0.3, 0.1]
+    sol = community.cooperative_tradeoff(fraction=fs)
+    assert sol.shape == (4, 2)
+    for i, f in sol.iterrows():
+        assert f.solution.growth_rate == approx(fs[i] * 0.874, 0.001)
+
+
+def test_benchmark_ct(community, benchmark):
+    benchmark(community.cooperative_tradeoff, fraction=1.0)

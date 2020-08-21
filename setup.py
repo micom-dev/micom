f1784377ce8c5d2050d3e62daeca1da6dfff7eb1
Christian Diener <ch.diener@gmail.com>
2020-08-21 12:45:34 -0700

    Bump version: 0.18.1 → 0.18.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.18.1",
+    version="0.18.2",
     description="Microbial community modeling based on cobrapy.",
     long_description=long_description,
     # The project's main homepage.

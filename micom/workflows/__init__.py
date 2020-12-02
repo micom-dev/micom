4a54d1416d68e1d97f6bb15029a1ac75bfd6e549
Christian Diener <ch.diener@gmail.com>
2020-12-02 11:45:48 -0800

    add db media checking

--- micom/workflows/__init__.py
+++ micom/workflows/__init__.py
@@ -5,11 +5,13 @@ from .build import build, build_database
 from .grow import grow
 from .tradeoff import tradeoff
 from .media import fix_medium, minimal_media
+from .db_media import check_db_medium
 
 __all__ = (
     "workflow",
     "build",
     "build_database",
+    "check_db_medium"
     "grow",
     "tradeoff",
     "fix_medium",

1fcd374819bb5fdb87c11d89d46f7dce02135e72
Christian Diener <ch.diener@gmail.com>
2023-10-29 12:53:07 -0700

    Bump version: 0.32.5 → 0.33.0

--- micom/__init__.py
+++ micom/__init__.py
@@ -35,4 +35,4 @@ __all__ = (
     "show_versions",
 )
 
-__version__ = "0.32.5"
+__version__ = "0.33.0"
--- setup.py
+++ setup.py
@@ -5,4 +5,4 @@ This is a shim left here for compatibility reasons.
 
 from setuptools import setup
 
-setup(version="0.32.5")
+setup(version="0.33.0")

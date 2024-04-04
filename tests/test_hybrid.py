36c0bc9a0bfdea57dcef0d1b348d234cc979e4d3
Christian Diener <ch.diener@gmail.com>
2024-04-04 21:09:07 +0200

    port tests to hybrid solver

--- tests/test_hybrid.py
+++ tests/test_hybrid.py
@@ -1,6 +1,6 @@
-"""Test the OSQP solver.
+"""Test the hybrid solver.
 
-We only test the core functions with relaxed accuracies as OSQP is not good with LPs.
+We only test the core functions with relaxed accuracies as hybrid is not good with LPs.
 """
 
 from .fixtures import community
@@ -19,8 +19,8 @@ import pytest
 from pytest import approx
 
 pytestmark = pytest.mark.skipif(
-    "osqp" not in su.solvers,
-    reason="OSQP not functional here"
+    "hybrid" not in su.solvers,
+    reason="hybrid not functional here"
 )
 
 medium = load_qiime_medium(md.test_medium)
@@ -30,7 +30,7 @@ db = md.test_db
 @pytest.mark.parametrize("strategy", ["none", "minimal imports", "pFBA"])
 def test_grow(tmp_path, strategy):
     data = md.test_data()
-    built = build(data, db, str(tmp_path), cutoff=0, solver="osqp")
+    built = build(data, db, str(tmp_path), cutoff=0, solver="hybrid")
     grown = grow(built, str(tmp_path), medium, 0.5, strategy=strategy)
     assert len(grown) == 3
     assert "growth_rate" in grown.growth_rates.columns
@@ -41,7 +41,7 @@ def test_grow(tmp_path, strategy):
 
 def test_tradeoff(tmp_path):
     data = md.test_data()
-    built = build(data, db, str(tmp_path), cutoff=0, solver="osqp")
+    built = build(data, db, str(tmp_path), cutoff=0, solver="hybrid")
     rates = tradeoff(built, str(tmp_path), medium)
     assert "growth_rate" in rates.columns
     assert "tradeoff" in rates.columns
@@ -50,7 +50,7 @@ def test_tradeoff(tmp_path):
 
 def test_media(tmp_path):
     data = md.test_data()
-    built = build(data, db, str(tmp_path), cutoff=0, solver="osqp")
+    built = build(data, db, str(tmp_path), cutoff=0, solver="hybrid")
     media = minimal_media(built, str(tmp_path), 0.5)
     assert media.shape[0] > 3
     assert "flux" in media.columns
@@ -59,7 +59,7 @@ def test_media(tmp_path):
 
 def test_fix_medium(tmp_path):
     data = md.test_data()
-    built = build(data, db, str(tmp_path), cutoff=0, solver="osqp")
+    built = build(data, db, str(tmp_path), cutoff=0, solver="hybrid")
     bad_medium = medium.iloc[0:2, :]
     fixed = fix_medium(built, str(tmp_path), bad_medium, 0.5, 0.001, 10)
     assert fixed.shape[0] > 3
@@ -67,7 +67,7 @@ def test_fix_medium(tmp_path):
 
 
 def test_community_objective(community):
-    community.solver = "osqp"
+    community.solver = "hybrid"
     x = community.optimize()
     y = community.optimize(fluxes=True)
     assert isinstance(x, CommunitySolution)
@@ -78,14 +78,14 @@ def test_community_objective(community):
 
 
 def test_cooperative_tradeoff(community):
-    community.solver = "osqp"
+    community.solver = "hybrid"
     sol = community.cooperative_tradeoff(fraction=1.0)
     for g in sol.members.growth_rate.dropna():
         assert g == approx(0.874, 1e-2, 1e-2)
 
 
 def test_multiple_tradeoffs(community):
-    community.solver = "osqp"
+    community.solver = "hybrid"
     fs = [1.0, 0.5, 0.3, 0.1]
     sol = community.cooperative_tradeoff(fraction=fs)
     assert sol.shape == (4, 2)

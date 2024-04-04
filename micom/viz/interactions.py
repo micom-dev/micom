820bf7eb58ddd0145cef72bbb247419510c0d2c8
Christian Diener <ch.diener@gmail.com>
2024-04-04 20:48:11 +0200

    add mes

--- micom/viz/interactions.py
+++ micom/viz/interactions.py
@@ -1,10 +1,11 @@
 """Visualizations for interactions."""
 
 from datetime import datetime
-from micom.interaction import interactions, summarize_interactions
+from micom.interaction import interactions, summarize_interactions, MES
 from micom.logger import logger
 from micom.workflows import GrowthResults
 from micom.viz.core import Visualization
+import pandas as pd
 import re
 
 UNITS = {
@@ -67,3 +68,56 @@ def plot_focal_interactions(
         unit=UNITS[kind]
     )
     return viz
+
+
+def plot_mes(
+    results : GrowthResults,
+    taxon : str,
+    filename : str = "mes_%s.html" % datetime.now().strftime("%Y%m%d"),
+    metadata : pd.DataFrame = None
+) -> None:
+    """Plot metabolic interactions between a focal taxa and all other taxa.
+
+    This will visualize metabolic interaction between a taxon of interest (focal taxon)
+    and all other taxa across all samples.
+
+    Parameters
+    ----------
+    results : micom.workflows.GrowthResults
+        The results returned by the `grow` workflow.
+    taxon : str
+        The focal taxon to use as a reference. Must be one of the taxa appearing
+        in `results.growth_rates.taxon`.
+    filename : str
+        The HTML file where the visualization will be saved.
+    metadata : pamdas.DataFrame
+        Additional metadata to stratify MES score. Must contain a column called
+        `sample_id` and other categorical columns.
+
+    Returns
+    -------
+    Visualization
+        A MICOM visualization. Can be served with `viz.serve`.
+    """
+    if metadata is not None:
+        if "sample_id" not in metadata.columns:
+            raise ValueError("Metadata must contain a column named `sample_id`.")
+        categorical = metadata.drop(columns=["sample_id"]).select_dtypes(
+            include=["object", "category", "bool"]).columns
+        if len(categorical) == 0:
+            raise ValueError("Metadata contains no categorical columns.")
+    tol = results.exchanges.tolerance.max()
+    scores = MES(results, tol)
+    scores = pd.merge(scores, metadata[categorical], on="sample_id")
+    n_mets = scores.metabolite.nunique()
+    data = {"scores": scores}
+    viz = Visualization(filename, data, "scores.html")
+    viz.save(
+        scores=scores.to_json(orient="records"),
+        n_mets=n_mets,
+        name="Metabolic Exchange Score (MES)",
+        col_name="mes",
+        categories=", ".join(categorical)
+    )
+    return viz
+
